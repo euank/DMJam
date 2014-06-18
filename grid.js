079fa4f4360abba8128437d9a0b1c865683cc5f2
Jason Bury <jasondougbury@gmail.com>
2014-06-18 01:10:02 -0700

    Silly coding style things

--- grid.js
+++ grid.js
@@ -10,17 +10,17 @@ document.body.appendChild(grid);
 function clickableGrid(rows, cols, callback){
 	var grid = document.createElement('table');
 	grid.className = 'grid';
-	for (var r=0;r<rows;++r){
+	for (var r = 0; r < rows; r++){
 		var tr = grid.appendChild(document.createElement('tr'));
 
-		for(var c=0;c<cols;++c){
-			var cell = tr.appendChild(document.createElement('td'));
+		for(var c = 0; c < cols; c++){
+			var td = tr.appendChild(document.createElement('td'));
 			
-			cell.addEventListener('click', (function(element){
+			td.addEventListener('click', (function(element){
 				return function(){
 					callback(element);
 				}
-			})(cell),false);
+			})(td),false);
 		}
 	}
 	return grid;
